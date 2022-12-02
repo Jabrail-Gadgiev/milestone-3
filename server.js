b8a48aa0bc1baa676af33cbd2fd49aad0e3f48a8
IbrahimRJ <84452680+ibrahimrj@users.noreply.github.com>
2022-12-02 22:14:53 +0400

    Required and add the routes for property

--- server.js
+++ server.js
@@ -1,38 +1,36 @@
-const express = require('express');
+const express = require("express");
 const server = express();
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-require('dotenv').config();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const propertyRoute = require("./routes/property-routes");
+require("dotenv").config();
 
 // Configure middleware for express
-server.use(bodyParser.urlencoded({extended: false}));
+server.use(bodyParser.urlencoded({ extended: false }));
 server.use(bodyParser.json());
+
+server.use("/property", propertyRoute);
 
 const dbURL = process.env.DB_URL;
 
 const dbConfig = {
-    'useNewUrlParser': true,
-    'useUnifiedTopology': true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 };
 
 mongoose
-.connect(dbURL, dbConfig)
-.then(function() {
-    console.log('DB is connected');
-})
-.catch(function(connectionError) {
-    console.log('DB connection error', connectionError);
+  .connect(dbURL, dbConfig)
+  .then(function () {
+    console.log("DB is connected");
+  })
+  .catch(function (connectionError) {
+    console.log("DB connection error", connectionError);
+  });
+
+server.get("/", function (req, res) {
+  res.send("Hello World");
 });
 
-server.get('/',
-    function(req, res) {
-        res.send('Hello World')
-    }
-);
-
-server.listen(
-    process.env.PORT, 
-    function() {
-        console.log('Running on http://localhost:3002/')
-    }
-);
+server.listen(process.env.PORT, function () {
+  console.log("Running on http://localhost:3002/");
+});
